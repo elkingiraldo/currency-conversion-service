93ab1ae0b510a09e8014b1e07234a341cba1ad37
Elkin Giraldo <elkin.giraldo@telintel.net>
2018-03-14 11:26:13 -0500

    add log

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrenyConversionController.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrenyConversionController.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,8 @@ public class CurrenyConversionController {
 	
 	@Autowired
 	private CurrencyExchangeServiceProxy proxy;
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
@@ -45,6 +49,8 @@ public class CurrenyConversionController {
 		// ***** Start call service *****
 		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
 		// ***** Finish call service *****
+		
+		logger.info("{}", response);
 		
 		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
 				quantity.multiply(response.getConversionMultiple()), response.getPort());
